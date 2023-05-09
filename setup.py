77e5824e5a271aa256f86eb65e0a28147ccf2001
Josh Ehlinger <joshe@aweber.com>
2023-05-08 20:35:51 -0400

    Clarify version

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
